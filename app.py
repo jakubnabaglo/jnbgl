85f6bad12454e2bd7829ee6adf2a441e4438716c
Jakub Nabaglo <jaknab@me.com>
2017-06-14 19:03:22 +1000

    Force SSL

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import flask
 app = flask.Flask(__name__)
 
 
+@app.before_request
+def before_request():
+    if flask.request.url.startswith('http://'):
+        url = 'https://' + flask.request.url[7:]
+        return flask.redirect(url, code=301)
+
+
 @app.route('/')
 def home():
     return 'Hello there.'
