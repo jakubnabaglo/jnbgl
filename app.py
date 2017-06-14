1b02f96c1de4590142f887576b853bede8514e20
Jakub Nabaglo <jaknab@me.com>
2017-06-14 19:21:07 +1000

    Force HTTPS, attempt 2

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ import flask
 
 app = flask.Flask(__name__)
 
-
-@app.before_request
-def before_request():
-    if flask.request.url.startswith('http://'):
-        url = 'https://' + flask.request.url[7:]
-        return flask.redirect(url, code=301)
+if __name__ != '__main__':
+    @app.before_request
+    def before_request():
+        if flask.request.header['x-forwarded-proto'] != 'https':
+            flask.redirect('https://{}{}'.format(
+                flask.request.header['host'],
+                flask.request.path))
 
 
 @app.route('/')
 def home():
+    raise Exception()
     return 'Hello there.'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5001, debug=True)
