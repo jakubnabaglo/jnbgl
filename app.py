7054b81a2fc81100731bf518dd56320c49cc850e
Jakub Nabaglo <jaknab@me.com>
2017-06-18 20:48:50 +1000

    Hutter

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 import flask
 
 app = flask.Flask(__name__)
@@ -18,9 +20,25 @@ def home():
     return flask.render_template('home.html')
 
 
-@app.route('/projects')
+@app.route('/projects/')
 def projects():
-    return flask.render_template('projects.html')
+    ken = random.choices([False, True], cum_weights=[.8, 1])[0]
+    return flask.render_template('projects.html', ken=ken)
+
+
+@app.route('/projects/redshift/')
+def photometric_redshift():
+    return flask.render_template('redshift.html')
+
+
+@app.route('/projects/voting/')
+def homomorphic_counting():
+    return flask.render_template('voting.html')
+
+
+@app.route('/hutter/')
+def hutter():
+    return flask.render_template('hutter.html')
 
 
 if __name__ == '__main__':
